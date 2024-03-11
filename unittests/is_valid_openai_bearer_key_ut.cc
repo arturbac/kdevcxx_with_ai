e0772797481636d3e377e127d8dea229842a7364
Artur Bać <artur@ebasoft.com.pl>
2024-03-11 06:11:36 +0100

    remove key

--- unittests/is_valid_openai_bearer_key_ut.cc
+++ unittests/is_valid_openai_bearer_key_ut.cc
@@ -12,8 +12,7 @@ int main()
   "is_valid_openai_bearer_key"_test = []
   {
     expect(is_valid_openai_bearer_key("0123456789ABCDEF0123456789ABCDEF01234567") == true);
-    expect(is_valid_openai_bearer_key("0123456789ABCDEF0123456789ABCDEF0123456") == false);             // Too short
-    expect(is_valid_openai_bearer_key("") == false);                                                    // Empty string
-    expect(is_valid_openai_bearer_key("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") == true);  // Mixed case
+    expect(is_valid_openai_bearer_key("0123456789ABCDEF0123456789ABCDEF0123456") == false);  // Too short
+    expect(is_valid_openai_bearer_key("") == false);                                         // Empty string
   };
   }
